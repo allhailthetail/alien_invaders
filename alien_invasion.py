46af327d8ce1a56208dd7d379275ed9dd6d382c5
allhailthetail <matthewyounger33@gmail.com>
2024-04-16 19:36:03 -0500

    Refactoring code

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,37 +15,42 @@ class AlienInvasion:
         '''Initialize the game, create game resources.'''
         
         pygame.init()
-        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         self.clock = pygame.time.Clock()
         pygame.display.set_caption('Alien Invasion')
 
         # Set background color for the game:
         self.bg_color = (self.settings.bg_color)
-
         # Ship requires ai_game in order to initiate.
         # Here, we pass in self in order to provide this to Ship. 
         # In return, we get back an instance of ship, tied to the given object of AlienInvasion.
         self.ship = Ship(self)
 
+    def _check_events(self):
+        '''Respond to keypresses and mouse events'''
+
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+    def _update_screen(self):
+        '''Update images on the screen and flip to a new screen'''
+        
+        # Fill entire screen with background color:
+        self.screen.fill(self.bg_color)
+        # Allow the Ship to print to the screen
+        self.ship.blitme()
+        # Make the most recent screen visible:
+        pygame.display.flip()
+
     def run_game(self):
         '''Start the main loop for the game.'''
         
         while True:
-            # Watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            
-            # Fill entire screen with background color:
-            self.screen.fill(self.bg_color)
-
-            # Allow the Ship to print to the screen
-            self.ship.blitme()
-
-            # Make the most recent screen visible:
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()            
             # pygame will do best-effort to keep the game's framerate at 60fps
             self.clock.tick(60)
 
