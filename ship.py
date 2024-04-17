9ec4f4f1d1ea10deca59f57b5f494536819261f6
allhailthetail <matthewyounger33@gmail.com>
2024-04-16 20:07:35 -0500

    adding speed multiplier

--- ship.py
+++ ship.py
@@ -11,14 +11,18 @@ class Ship:
         # of a new instance of this class.  The idea here is they'll be easier to reference for new methods
         # within the scope of this class.  :)
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship and establish rect for it:
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()        
         
-        #Ship gets centered at middle-bottom of screen as initial condition
+        # Ship gets centered at middle-bottom of screen as initial condition
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a float of the ship's horizontal position
+        self.x = float(self.rect.x)
 
         # Start with a ship not moving
         self.moving_right = False
@@ -28,9 +32,13 @@ class Ship:
         '''Update ship's position based on the movement flag'''
 
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        # Update the self.rect.x value with new value.
+        # Note that only the int portion is kept
+        self.rect.x = self.x
 
     def blitme(self):
         '''Draw the ship at its current location.'''
