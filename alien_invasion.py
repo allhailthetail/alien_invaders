2fca8642161d8ec82386bd38eb2e41e3c292ef17
allhailthetail <matthewyounger33@gmail.com>
2024-04-20 13:15:20 -0500

    added bullets to game

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,8 +5,8 @@ import pygame
 import pygame 
 
 from settings import Settings
-
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     '''Overall class to manage game assets and behavior.'''
@@ -27,6 +27,7 @@ class AlienInvasion:
         # Here, we pass in self in order to provide this to Ship. 
         # In return, we get back an instance of ship, tied to the given object of AlienInvasion.
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def _check_events(self):
         '''Respond to keypresses and mouse events'''
@@ -45,8 +46,18 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
         elif event.key == pygame.K_q:
             sys.exit()
+    
+    def _fire_bullet(self):
+        '''Create a new bullet and add to group'''
+        new_bullet = Bullet(self)
+
+        # Limit number of bullets fired for a challenge:
+        if len(self.bullets) < self.settings.bullets_allowed:
+            self.bullets.add(new_bullet)
 
     def _check_keyup_events(self,event):
         '''Respond to key releases'''
@@ -59,16 +70,29 @@ class AlienInvasion:
         '''Update images on the screen and flip to a new screen'''
         # Fill entire screen with background color:
         self.screen.fill(self.bg_color)
+        # Update bullets:
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         # Allow the Ship to print to the screen
         self.ship.blitme()
         # Make the most recent screen visible:
         pygame.display.flip()
+
+    def _update_bullets(self):
+        '''Update bullets helper method'''
+        self.bullets.update()
+        # Get rid of bullets that have left the screen:
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+            print(len(self.bullets)) 
 
     def run_game(self):
         '''Start the main loop for the game.'''
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()            
             # pygame will do best-effort to keep the game's framerate at 60fps
             self.clock.tick(60)
